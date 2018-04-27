4578e8513309f37f1a537c748023c659f2be1427
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-04-27 13:45:11 -0600

    kick,anotherRock,Brass Knuckles, var name, var hits, working on damage

--- game.js
+++ game.js
@@ -1,6 +1,35 @@
 var health=100;
+var name=" Rocky ";
+var hits=0;
 function slap(){
-    health-1
-    alert(health);
+    health--
+    //alert(health);
+    update()
 }
-slap();
+
+function damage(){
+    hits++
+}
+
+function update(){
+    //goes to html . gets the element with id 'health' . display health value
+    document.getElementById("health").innerText = health
+    document.getElementById("name").innerText=name
+    document.getElementById("hits").innerText=hits
+
+}
+update();
+
+function kick() {
+    health=health-5
+    update()
+}
+
+function anotherRock(){
+    health=health-10
+    update()
+}
+function brassKnuckles(){
+    health=health-100
+    update()
+}
