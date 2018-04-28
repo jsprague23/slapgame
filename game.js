844df64e0e3872770a26f9affbb0fcc0e0d95aa0
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-04-27 23:17:38 -0600

    before reverting to older version

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
+ function Item(name,modifier,description){
+    this.name=name;
+    this.modifier=modifier;
+    this.description=description;
+ 
+var items = {
+    // arm:new Item("armReplace",0.3,"This changes the broken arm to a shiny new one.")
+    mittens: new Item("mittens",0.3,"Reduces the damage from punching")
+}    
+
+}
 var health=100.00;
-var name=" Rocky ";
+var named="Rocky";
 var hits=0;
 function slap(){
     health--
@@ -15,10 +26,9 @@ function damage(){
 function update(){
     //goes to html . gets the element with id 'health' . display health value
     document.getElementById("health").innerText = health
-    document.getElementById("name").innerText=name
+    document.getElementById("named").innerText=name
     document.getElementById("hits").innerText=hits
-
-}
+    
 update();
 
 function kick() {
@@ -42,4 +52,18 @@ function feather(){
     health=health-0.25
     update()
     damage()
+}
+function armReplace(){
+    health=health //health regen
+    update()
+}
+
+function mittens(){
+    health=health //damage reducer
+    update()
+}
+
+function oilChange(){
+    health=health+10
+    update()
 }
